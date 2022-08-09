d3a1ccb9f587a7e7dae12ac53d3748e5d9cc2c78
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-08-10 01:57:22 +0530

    third code

--- src/routes/route.js
+++ src/routes/route.js
@@ -18,9 +18,9 @@ router.get('/test-me', function (req, res) {
     let month= ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"]
     let result = lodash.chunk(month,3)
     console.log(result);
+    
     let oddNum= [1,3,5,7,9,11,13,15,17,19]
-    let results = lodash.tail(9)
-    console.log(results)
+    console.log(lodash.tail(oddNum))
    });
 router.get('/test-me', function(req, res){
     res.send('This is the second routes implementation')
@@ -29,5 +29,28 @@ router.get('/test-me', function(req, res){
 router.get('/give-me-students-data',function(req, res){
 
 })
+router.get('/movies', function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(movies);
+    res.send(movies);
+});
+router.get("/movies/:indexNumber", function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+    let ourmovie = movies[movieIndex]
+    console.log(ourmovie)
+    res.send(ourmovie)
+});
+router.get("/movies/:indexNumber", function (req, res) {
+    const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    console.log(req.params.indexNumber)
+    let movieIndex = req.params.indexNumber
+
+    if (movieIndex < 0 || movieIndex >= movies.length) {
+        return req.send('invalid input try again')
+    }
+    let ourmovie = movies[movieIndex]
+    res.send(ourmovie)
 module.exports = router;
 // adding this comment for no reason
