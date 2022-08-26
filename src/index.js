3632aabadba3ca683ba7d3ba681174a54264b323
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-08-26 17:22:55 +0530

    jsdg

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ziyaurrahman:google79821@cluster0.xuxdo5g.mongodb.net/Rahman-database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
