acb3c4515067c08d3e8ae994320ced6b28feb92a
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-08-30 15:28:19 +0530

    middleware2 code

---
+++ src/middlewares/myMiddlewares.js
@@ -0,0 +1,9 @@
+const headerCheck = function(req,res,next){
+    if(req.headers.isfreeappuser)  
+     next()
+    else   
+     res.send("Please provide header")
+}
+
+
+module.exports.headerCheck= headerCheck
