9e05a2cc6a599a94b53252ffd8413814e776b74c
ziyaurrahman01 <ziyaurrahmankhan01@gmail.com>
2022-08-31 17:53:52 +0530

    this is my auth-2 assignment

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ziyaurrahman:google79821@cluster0.xuxdo5g.mongodb.net/auth-2-database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
+
 
 app.use('/', route)
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,33 @@
-const authenticate = function(req, req, next) {
+// const jwk=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+
+const authenticate = function (req, res, next) {
     //check the token in request header
     //validate this token
+    //   let token = req.headers["x-Auth-token"] //let token = req.headers["x-Auth-token"];
 
+     let token = req.headers['x-Auth-token'];
+    if (!token) token = req.headers['x-auth-token'];//case senstive
+    if (!token) return res.send({ status: false, msg: "token must be present" })
+    console.log(token);
+    let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+     if (!decodedToken){
+   return res.send({ status: false, msg: "token is invalid" });
+    }
+    req.loggedInUser = decodedToken.userId
     next()
 }
 
 
-const authorise = function(req, res, next) {
+const authorise = function (req, res, next) {
     // comapre the logged in user's id and the id in request
+    let userToBeModified = req.params.userId//user access userId 
+    //let userloggedin = jwkdecoder.userId//decorder userId
+    if (userToBeModified !=req.loggedInUser) {
+        res.send({ status: false, msg: "userLoggedIn is not allowed " })
+    }
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
